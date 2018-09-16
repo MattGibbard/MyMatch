643348ae2e82cc0c9ba68cd4ee7604f66922aa0e
MattGibbard <matt.gibbard@hotmail.com>
2018-09-16 17:28:10 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-//test
+//Add requires
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+//Set global variables
+let port = process.env.PORT || 8080;
+
+//Allow app to use Public directory for CSS
+app.use(express.static(__dirname + '/public'));
+//Allow app to use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Set view engine to EJS
+app.set('view engine', 'ejs');
+
+//Homepage get request
+app.get('/', function(req, res) {
+    res.render('pages/home.ejs');
+});
+
+//Listen server
+app.listen(port, function() {
+    console.log('Server started and listening on port ' + port);
+});
